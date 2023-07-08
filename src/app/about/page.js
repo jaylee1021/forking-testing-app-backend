caef952535ce5de575ae6edc73961a22de7986d9
Kacy Phan <kacyphann@gmail.com>
2023-07-08 15:03:59 -0700

    feat: add h3

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,6 +3,7 @@ export default function About() {
         <>
             <h1>This is about page</h1>
             <h2>This is about page 2.0</h2>
+            <h3>This is about page 3.0</h3>
         </>
     );
 }
